162cd5f1e65b14dd329ed1fd0ef4195a5e7e78d1
Cody Kochmann <kochmanncody@gmail.com>
2017-11-09 11:59:03 -0500

    first official release

--- setup.py
+++ setup.py
@@ -2,19 +2,19 @@
 # @Author: Cody Kochmann
 # @Date:   2017-11-09 11:50:41
 # @Last Modified 2017-11-09
-# @Last Modified time: 2017-11-09 11:55:04
+# @Last Modified time: 2017-11-09 11:58:49
 
 from distutils.core import setup
 
 setup(
   name = 'strict_functions',
   packages = ['strict_functions'], # this must be the same as the name above
-  version = '2017.11.9',
+  version = '2017.11.9.1',
   description = 'collection of helpful strict_functions that should have been in itertools',
   author = 'Cody Kochmann',
   author_email = 'kochmanncody@gmail.com',
   url = 'https://github.com/CodyKochmann/strict_functions',
-  download_url = 'https://github.com/CodyKochmann/strict_functions/tarball/2017.11.9',
+  download_url = 'https://github.com/CodyKochmann/strict_functions/tarball/2017.11.9.1',
   keywords = ['strict_functions', 'type', 'enforce', 'strict', 'restrict', 'strict defaults', 'defaults'],
   classifiers = [],
 )
