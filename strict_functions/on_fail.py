b1d69532db31dd22a22b5463a2078b279c0b3648
CodyKochmann <kochmanncody@gmail.com>
2017-11-10 10:22:00 -0500

    added on_fail

--- strict_functions/on_fail.py
+++ strict_functions/on_fail.py
@@ -3,6 +3,7 @@ from functools import wraps
 class on_fail(object):
 	""" this is a decorator that adds a default output if a function fails """
 	def __init__(self, default_output, logger=None):
+		assert logger is None or callable(logger), 'logger needs to be a callable function'
 		self.default_output = default_output
 		self.logger = logger
 
@@ -14,7 +15,7 @@ class on_fail(object):
 			except Exception as ex: # if it fails
 				if self.logger is not None:
 					# log the error if a logger was defined
-					self.logger.write(repr(ex))
+					self.logger(repr(ex))
 				# then return the default output
 				return self.default_output
 		return wrapper
@@ -28,9 +29,9 @@ if __name__ == '__main__':
 	print(my_function(1,2,3))
 	print(my_function(1,2,'fish'))
 
-	import sys
+	import logging
 
-	@on_fail('waffles', logger=sys.stdout)
+	@on_fail('waffles', logger=logging.warning)
 	def my_function(a,b,c):
 		return a*b/c
 
