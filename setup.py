4b4d992ce80ecc64d443226d00ef81ff90ea1329
Cody Kochmann <kochmanncody@gmail.com>
2018-02-06 18:24:28 -0500

    added c_version decorator

--- setup.py
+++ setup.py
@@ -9,12 +9,13 @@ from distutils.core import setup
 setup(
   name = 'strict_functions',
   packages = ['strict_functions'], # this must be the same as the name above
-  version = '2017.11.22.2',
+  version = '2017.2.6',
   description = 'collection of helpful strict_functions that should have been in itertools',
   author = 'Cody Kochmann',
   author_email = 'kochmanncody@gmail.com',
+  install_requires = ['cffi'],
   url = 'https://github.com/CodyKochmann/strict_functions',
-  download_url = 'https://github.com/CodyKochmann/strict_functions/tarball/2017.11.22.2',
+  download_url = 'https://github.com/CodyKochmann/strict_functions/tarball/2017.2.6',
   keywords = ['strict_functions', 'type', 'enforce', 'strict', 'restrict', 'strict defaults', 'defaults'],
   classifiers = [],
 )
