adaf78084c66929552d80c95f980e7e0c4331478
Cody Kochmann <kochmanncody@gmail.com>
2018-12-23 20:14:13 -0500

    added the imports

--- strict_functions/__init__.py
+++ strict_functions/__init__.py
@@ -25,6 +25,15 @@ if sys.version_info >= (3,0):
     from trace3 import trace
 else:
     from trace2 import trace
+try:
+    from lru_cache3 import lru_cache
+except:
+    from lru_cache2 import lru_cache
+try:
+    from cached3 import cached
+except:
+    from cached2 import cached
+
 
 sys.path.remove(os.path.dirname(__file__))
 
